6c861bc5e6258242895f01422e5e0a2e1790fa7a
Rohith Reddy <rohithreddy2219@gmail.com>
2017-03-11 16:24:16 +0530

    Added a file which computes centrality measures

---
+++ code/gen_centrality_stats.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 14:17:22 2016
+
+@author: rohithreddy
+
+This script calculates the betweenness paramter for each edge and
+stores it in the database.
+"""
+from graphs import *
+from common import *
+from plot_functions import *
+db = sys.argv[1];
+table_e = 'cleaned_ways'
+table_v = 'cleaned_ways_vertices_pgr'
+
+d = {}
+d["db"] = db;
+d["fraction"] = 0.5
+d["table_e"] = table_e
+d["table_v"] = table_v
+d["column"] = "id";
+conn = psycopg2.connect(database=d['db'], user="postgres", password="postgres", host="127.0.0.1", port="5432")
+d['conn'] = conn
+
+# Generating edge count based on betweenness
+print "Generating edge count...."
+d["column"] = "id";
+d['table'] = "contracted_ways_vertices_pgr";
+d['num_vertices'] = get_count(d);
+d['table'] = "contracted_ways";
+d['num_edges'] = get_count(d);
+d['num_pairs'] = long(d['fraction']*d['num_vertices']*d['num_vertices']);
+d["count"]= generate_edge_count(d);
+
+#Updating the betweenness column
+d["column"]="betweenness"
+d["table"] = table_e;
+update_column(d);
+
+#Saving the betweenness distribution
+d['betweenness_values'] = [c for c in d['count'].values() if c > 0]
+d['column'] = 'betweenness'
+betweenness_distribution(d)
+
+
+conn.commit()
+conn.close()
+
+
+#Now our skeleton is ready
+
+
+
+
+
+
