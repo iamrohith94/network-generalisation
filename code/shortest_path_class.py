001ba2a3d0e5d4b77bdc623328e2d3b69cf93b69
Rohith Reddy <rohithreddy2219@gmail.com>
2017-03-24 13:50:54 +0530

    Modified path class

--- code/shortest_path_class.py
+++ code/shortest_path_class.py
@@ -9,203 +9,208 @@ class ShortestPath:
 	password = "postgres"
 	host="127.0.0.1"
 	port="5432"
-	vertex_table = "cleaned_ways_vertices_pgr";
-	edge_table = "cleaned_ways";
-	target_level = 1;
+	vertex_table = "cleaned_ways_vertices_pgr"
+	edge_table = "cleaned_ways"
+	target_level = 1
 	def __init__(self, db):
-		self.db = db;
+		self.db = db
 		self.conn = psycopg2.connect(database=db, user=self.user, password=self.password, host=self.host, port=self.port)
-		self.cur = self.conn.cursor();
+		self.cur = self.conn.cursor()
 
 	def get_node_level(self, node_id, level_column):
-		query = "SELECT %s FROM %s WHERE id = %s";
-		self.cur.execute(query, (AsIs(level_column), AsIs(self.vertex_table), node_id,));
-		rows = self.cur.fetchall();
+		query = "SELECT %s FROM %s WHERE id = %s"
+		self.cur.execute(query, (AsIs(level_column), AsIs(self.vertex_table), node_id,))
+		rows = self.cur.fetchall()
 		for row in rows:
-			return row[0];
+			return row[0]
 
 	def get_next_node(self, curr_node_id, target_node_id, level_column, visited_nodes, forward):
 		if self.get_node_level(curr_node_id, level_column) == self.target_level:
-			return curr_node_id;
+			return curr_node_id
 		if forward:
-			return self.forward_level_heuristic(curr_node_id, target_node_id, level_column, visited_nodes);
-		return self.backward_level_heuristic(curr_node_id, target_node_id, level_column, visited_nodes);
+			return self.forward_level_heuristic(curr_node_id, target_node_id, level_column, visited_nodes)
+		return self.backward_level_heuristic(curr_node_id, target_node_id, level_column, visited_nodes)
 
 	def update_path_cost(self, path):
-		query_1 = "SELECT cost FROM %s WHERE id = %s AND source = %s";
-		query_2 = "SELECT reverse_cost FROM %s WHERE id = %s AND target = %s";
+		query_1 = "SELECT cost FROM %s WHERE id = %s AND source = %s"
+		query_2 = "SELECT reverse_cost FROM %s WHERE id = %s AND target = %s"
 		for entry in path.get_path():
 			if entry['edge'] != -1:
-				self.cur.execute(query_1, (AsIs(self.edge_table), entry['edge'], entry['node']));
-				rows = self.cur.fetchall();
+				self.cur.execute(query_1, (AsIs(self.edge_table), entry['edge'], entry['node']))
+				rows = self.cur.fetchall()
 				for row in rows:
-					entry['cost'] = row[0];
-				self.cur.execute(query_2, (AsIs(self.edge_table), entry['edge'], entry['node']));
-				rows = self.cur.fetchall();
+					entry['cost'] = row[0]
+				self.cur.execute(query_2, (AsIs(self.edge_table), entry['edge'], entry['node']))
+				rows = self.cur.fetchall()
 				for row in rows:
-					entry['cost'] = row[0];
-		return path;
+					entry['cost'] = row[0]
+		return path
 
 
 	def get_path_to_target_level(self, curr_node_id, target_node_id, level_column, forward = True):
-		path = Path();
-		visited = [];
+		path = Path()
+		visited = []
 		if not forward:
-			path.add_entry({'node': curr_node_id, 'edge': -1, 'cost': 0.0000});
-		#print curr_node_id;
-		temp_node_id = curr_node_id;
+			path.add_entry({'node': curr_node_id, 'edge': -1, 'cost': 0.0000})
+		#print curr_node_id
+		temp_node_id = curr_node_id
 		while self.get_node_level(temp_node_id, level_column) != self.target_level:
-			visited.append(temp_node_id);
-			temp_node_id, entry = self.get_next_node(temp_node_id, target_node_id, level_column, visited, forward);
-			path.add_entry(entry);
-			print entry;
-			print temp_node_id;
+			visited.append(temp_node_id)
+			temp_node_id, entry = self.get_next_node(temp_node_id, target_node_id, level_column, visited, forward)
+			path.add_entry(entry)
+			print entry
+			print temp_node_id
 		if forward:
-			path.add_entry({'node': temp_node_id, 'edge': -1, 'cost': 0.0000});
+			path.add_entry({'node': temp_node_id, 'edge': -1, 'cost': 0.0000})
 		else:
-			path.get_path().reverse();
-		return path;
+			path.get_path().reverse()
+		return path
 
 	def get_path_on_skeleton(self, source, target, level_column):
-		path = Path();
+		path = Path()
 		inner_query = "SELECT id, source, target, cost, reverse_cost \
-		FROM "+self.edge_table+" WHERE "+level_column+" <= "+str(self.target_level);
-		query = "SELECT node, edge, cost FROM pgr_dijkstra(%s, %s, %s)";
-		self.cur.execute(query, (inner_query, source, target));
-		rows = self.cur.fetchall();
+		FROM "+self.edge_table+" WHERE "+level_column+" <= "+str(self.target_level)
+		query = "SELECT node, edge, cost FROM pgr_dijkstra(%s, %s, %s)"
+		self.cur.execute(query, (inner_query, source, target))
+		rows = self.cur.fetchall()
 		for row in rows:
-			path.add_entry({'node': row[0], 'edge': row[1], 'cost': row[2]});
-		return path;
+			path.add_entry({'node': row[0], 'edge': row[1], 'cost': row[2]})
+		return path
 
 	def get_generalised_path(self, source, target, level_column):
-		source_to_skeleton = self.astar_path(source, target, level_column);
-		self.update_path_cost(source_to_skeleton);
+		source_to_skeleton = self.astar_path(source, target, level_column)
+		self.update_path_cost(source_to_skeleton)
+		"""
 		print "Source to skeleton: "+str(len(source_to_skeleton)-1)
-		print source_to_skeleton;
-
-		skeleton_to_target = self.astar_path(target, source_to_skeleton.get_last_node(), level_column, True);
-		self.update_path_cost(skeleton_to_target);
+		print source_to_skeleton
+		"""
+		skeleton_to_target = self.astar_path(target, source_to_skeleton.get_last_node(), level_column, True)
+		self.update_path_cost(skeleton_to_target)
+		"""
 		print "Skeleton to target: "+str(len(skeleton_to_target)-1)
-		print skeleton_to_target;
-
-		on_skeleton = self.get_path_on_skeleton(source_to_skeleton.get_last_node(), skeleton_to_target.get_start_node(), level_column);
+		print skeleton_to_target
+		"""
+		on_skeleton = self.get_path_on_skeleton(source_to_skeleton.get_last_node(), skeleton_to_target.get_start_node(), level_column)
+		"""
 		print "On Skeleton: "+str(len(on_skeleton))
-		print on_skeleton;
-		
-		final_path = Path();
-		final_path.add_path(source_to_skeleton);
-		final_path.add_path(on_skeleton);
-		final_path.add_path(skeleton_to_target);
+		print on_skeleton
+		"""
+		final_path = Path()
+		final_path.add_path(source_to_skeleton)
+		final_path.add_path(on_skeleton)
+		final_path.add_path(skeleton_to_target)
 
 		#print "Final Path: "+str(len(final_path))
-		#print final_path;
+		#print final_path
 
-		return final_path;
+		return final_path
 
 	def get_original_path(self, source, target):
-		path = Path();
+		path = Path()
 		inner_query = "SELECT id, source, target, cost, reverse_cost \
-		FROM "+self.edge_table;
-		query = "SELECT node, edge, cost FROM pgr_dijkstra(%s, %s, %s)";
-		self.cur.execute(query, (inner_query, source, target));
-		rows = self.cur.fetchall();
+		FROM "+self.edge_table
+		query = "SELECT node, edge, cost FROM pgr_dijkstra(%s, %s, %s)"
+		self.cur.execute(query, (inner_query, source, target))
+		rows = self.cur.fetchall()
 		for row in rows:
-			path.add_entry({'node': row[0], 'edge': row[1], 'cost': row[2]});
-		return path;
+			path.add_entry({'node': row[0], 'edge': row[1], 'cost': row[2]})
+		return path
 
 	def euclidean_distance(self, source, target):
-		#print source, target;
+		#print source, target
 		query = "SELECT ST_Distance(v1.the_geom, v2.the_geom) FROM %s AS v1, %s AS v2 \
-		WHERE v1.id = %s AND v2.id = %s";
-		self.cur.execute(query, (AsIs(self.vertex_table), AsIs(self.vertex_table), source, target));
-		rows = self.cur.fetchall();
+		WHERE v1.id = %s AND v2.id = %s"
+		self.cur.execute(query, (AsIs(self.vertex_table), AsIs(self.vertex_table), source, target))
+		rows = self.cur.fetchall()
 		for row in rows:
-			dist = row[0];
-		return dist;
+			dist = row[0]
+		return dist
 
 	def get_out_neighbors(self, node):
-		query_1 = "SELECT id, target, cost FROM %s WHERE cost > 0 AND source = %s";
-		query_2 = "SELECT id, source, reverse_cost FROM %s WHERE reverse_cost > 0 AND target = %s";
+		query_1 = "SELECT id, target, cost FROM %s WHERE cost > 0 AND source = %s"
+		query_2 = "SELECT id, source, reverse_cost FROM %s WHERE reverse_cost > 0 AND target = %s"
 		neighbors = set()
-		self.cur.execute(query_1, (AsIs(self.edge_table), node, ));
-		rows = self.cur.fetchall();
+		self.cur.execute(query_1, (AsIs(self.edge_table), node, ))
+		rows = self.cur.fetchall()
 		for row in rows:
-			neighbors.add((row[0], row[1], row[2],));
-		self.cur.execute(query_2, (AsIs(self.edge_table), node, ));
-		rows = self.cur.fetchall();
+			neighbors.add((row[0], row[1], row[2],))
+		self.cur.execute(query_2, (AsIs(self.edge_table), node, ))
+		rows = self.cur.fetchall()
 		for row in rows:
-			neighbors.add((row[0], row[1], row[2],));
-		return list(neighbors);
+			neighbors.add((row[0], row[1], row[2],))
+		return list(neighbors)
 
 	def get_in_neighbors(self, node):
-		query_1 = "SELECT id, source, cost FROM %s WHERE cost > 0 AND target = %s";
-		query_2 = "SELECT id, target, reverse_cost FROM %s WHERE reverse_cost > 0 AND source = %s";
+		query_1 = "SELECT id, source, cost FROM %s WHERE cost > 0 AND target = %s"
+		query_2 = "SELECT id, target, reverse_cost FROM %s WHERE reverse_cost > 0 AND source = %s"
 		neighbors = set()
-		self.cur.execute(query_1, (AsIs(self.edge_table), node, ));
-		rows = self.cur.fetchall();
+		self.cur.execute(query_1, (AsIs(self.edge_table), node, ))
+		rows = self.cur.fetchall()
 		for row in rows:
-			neighbors.add((row[0], row[1], row[2],));
-		self.cur.execute(query_2, (AsIs(self.edge_table), node, ));
-		rows = self.cur.fetchall();
+			neighbors.add((row[0], row[1], row[2],))
+		self.cur.execute(query_2, (AsIs(self.edge_table), node, ))
+		rows = self.cur.fetchall()
 		for row in rows:
-			neighbors.add((row[0], row[1], row[2],));
-		return list(neighbors);
+			neighbors.add((row[0], row[1], row[2],))
+		return list(neighbors)
 
 
 	def astar_path(self, source, target, level_column, reverse = False):
 		#print source, target
 		heuristic = self.euclidean_distance
-		path = Path();
-		nodes_in_queue = {};
-		explored_nodes = {};
+		path = Path()
+		explored_nodes = {}
+		nodes_in_queue = {}
 		#[node_level, total_dist, dist_from_source, node, parent_node, parent_edge]
-		source_level = self.get_node_level(source, level_column);
-		queue = [(source_level, 0, 0, source, None, None)];
+		source_level = self.get_node_level(source, level_column)
+		queue = [(source_level, 0, 0, source, None, None)]
 		push = heappush
    		pop = heappop
 		while queue:
-			level, _, dist_from_source, currnode, parent_node, parent_edge = pop(queue);
+			#print "queue", queue
+			level, _, dist_from_source, currnode, parent_node, parent_edge = pop(queue)
 			"""
-			print "Exploring Node:";
-			print "node: "+str(currnode);
-			print "parent_node: "+str(parent_node);
-			print "parent_edge: "+str(parent_edge);
+			print "Exploring Node:"
+			print "node: "+str(currnode)
+			print "parent_node: "+str(parent_node)
+			print "parent_edge: "+str(parent_edge)
 			"""
 			#if the currnode is target we backtrack using the parents and extract the path
-			if level == self.target_level or currnode == target:
+			if currnode == target or self.get_node_level(currnode, level_column) == self.target_level:
 				if reverse == True:
-					node = currnode;
-					explored_nodes[node] = (parent_node, parent_edge);
+					node = currnode
+					explored_nodes[node] = (parent_node, parent_edge)
 					while explored_nodes[node][1] != None:
-						path.add_entry({'node': node, 'edge': explored_nodes[node][1], 'cost': 0.0000});
-						node = explored_nodes[node][0];
-					path.add_entry({'node': node, 'edge': -1, 'cost': 0.0000});
+						path.add_entry({'node': node, 'edge': explored_nodes[node][1], 'cost': 0.0000})
+						node = explored_nodes[node][0]
+					path.add_entry({'node': node, 'edge': -1, 'cost': 0.0000})
 				else:
-					path.add_entry({'node': currnode, 'edge': -1, 'cost': 0.0000});
-					node = currnode;
-					explored_nodes[node] = (parent_node, parent_edge);
+					path.add_entry({'node': currnode, 'edge': -1, 'cost': 0.0000})
+					node = currnode
+					explored_nodes[node] = (parent_node, parent_edge)
 					while explored_nodes[node][0] != None:
-						path.add_entry({'node': explored_nodes[node][0], 'edge': explored_nodes[node][1], 'cost': 0.0000});
-						node = explored_nodes[node][0];
-						edge = explored_nodes[node][1];
-					path.reverse_path();
-				return path;
+						path.add_entry({'node': explored_nodes[node][0], 'edge': explored_nodes[node][1], 'cost': 0.0000})
+						node = explored_nodes[node][0]
+						edge = explored_nodes[node][1]
+					path.reverse_path()
+				return path
 
 			#if the current node is already explored we ignore it
 			if currnode in explored_nodes:
-				continue;
+				continue
 
 			#we put the node into the set of explored nodes
-			explored_nodes[currnode] = (parent_node, parent_edge);
+			explored_nodes[currnode] = (parent_node, parent_edge)
 			#print "Adding neighbors:"
 			#if the current node is not explored we add the neighbors to the queue
 			
 			if reverse == True:
-				to_explore = self.get_in_neighbors(currnode);
+				to_explore = self.get_in_neighbors(currnode)
 			else:
-				to_explore = self.get_out_neighbors(currnode);
+				to_explore = self.get_out_neighbors(currnode)
+
+			#print "neighbors: ", to_explore
 			for edge_id, neighbor, weight in to_explore:
-				
 				if neighbor in explored_nodes:
 					continue;
 				ncost = dist_from_source + weight;
@@ -216,10 +221,9 @@ class ShortestPath:
 				else:
 					h = heuristic(neighbor, target);
 				nodes_in_queue[neighbor] = ncost, h;
-				#print neighbor
+				#print "Pushing ", neighbor
 				push(queue, (self.get_node_level(neighbor, level_column), ncost+h, ncost, neighbor, currnode, edge_id));
-
-		return path;
+		return path
 
 
 		
