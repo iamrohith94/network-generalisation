5705f1c772b7172ed6d0aa06f2e99969e40b816a
Rohith Reddy <rohithreddy2219@gmail.com>
2017-08-11 11:07:37 +0530

    Added code to analyse the popular roads in skeleton

---
+++ src/popularity_evidence.py
@@ -0,0 +1,48 @@
+import psycopg2
+from psycopg2.extensions import AsIs
+import sys
+
+db = sys.argv[1];
+table_e = 'cleaned_ways'
+table_v = 'cleaned_ways_vertices_pgr'
+
+d = {}
+d["db"] = db;
+motorway = 101
+trunk = 104
+primary = 106
+secondary = 108
+tertiary = 109
+primary_link = 107
+secondary_link = 124
+tertiary_link = 125
+popular_ways = [motorway, trunk, primary, secondary, tertiary]
+
+conn = psycopg2.connect(database=d['db'], user="postgres", password="postgres", host="127.0.0.1", port="5432")
+
+levels = [10, 20, 30, 40, 50]
+
+#total_count_sql = "SELECT count(*) FROM %s WHERE promoted_level_%s = 1";
+total_count_sql = "SELECT count(*) FROM %s WHERE class_id = ANY(%s)"
+popular_count_sql = "SELECT count(*) FROM %s WHERE promoted_level_%s = 1 AND class_id = ANY(%s)"
+cur = conn.cursor()
+cur.execute(total_count_sql, (AsIs(table_e), popular_ways, ))
+rows = cur.fetchall()
+for row in rows:
+	total_count = row[0]
+for level in levels:
+	"""
+	cur.execute(total_count_sql, (AsIs(table_e), level, ))
+	rows = cur.fetchall()
+	for row in rows:
+		total_count = row[0]
+	"""
+	cur.execute(popular_count_sql, (AsIs(table_e), level, popular_ways, ))
+	rows = cur.fetchall()
+	for row in rows:
+		popular_count = row[0]
+
+	print "level: ", level
+	#print "total_count: ", total_count
+	#print "popular_count: ", popular_count
+	print "percent of popular roads: ", (popular_count*1.00/total_count)*100
---
+++ src/popularity_evidence.sql
@@ -0,0 +1,7 @@
+ALTER TABLE cleaned_ways ADD COLUMN class_id integer ;
+ALTER TABLE cleaned_ways ADD COLUMN type_id integer ;
+
+UPDATE cleaned_ways SET class_id = ways.class_id FROM ways WHERE ways.gid = id;
+
+UPDATE cleaned_ways SET type_id = osm_way_classes.type_id FROM osm_way_classes WHERE class_id = osm_way_classes.class_id;
+
